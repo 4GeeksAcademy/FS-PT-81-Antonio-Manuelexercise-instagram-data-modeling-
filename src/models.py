e80720ad20f7511e0dcbd29f385a4baeb95fa336
antomanu95 <antoniomr.estevez@hotmail.com>
2024-12-11 13:00:53 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -7,28 +7,62 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Followers(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key = True)
+    from_id = Column(Integer, ForeignKey('users.id'))
+    to_id = Column(Integer, ForeignKey('users.id'))
+    user_from_id = relationship('Users', foreign_keys=[from_id], backref = 'Follower')
+    user_to_id = relationship('Users', foreign_keys=[to_id], backref = 'Followed')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key = True)
+    email = Column(String, nullable = False, unique = True)
+    password = Column(String, nullable = False)
+    city = Column(String)
 
-    def to_dict(self):
-        return {}
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "city": self.city,
+            "posts": [ post.serialize() for post in self.posts] if self.posts else None
+        }
 
-## Draw from SQLAlchemy base
+class Posts(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key = True)
+    title = Column(String, nullable = False)
+    content = Column(String)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('Users', backref = 'posts')
+
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "posts": self.user_id
+        }
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key = True)
+    content = Column(String, nullable = False)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('Users', backref = 'comments')
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    post = relationship('Users', backref = 'comments')
+
+class Medias(Base):
+    __tablename__ = 'medias'
+    id = Column(Integer, primary_key = True)
+    src = Column(String, nullable = False)
+
+
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
